f8684b07bf812cd3d1cb13106a3f64107a948276
nicobascon <nico.bascon@live.fr>
2014-01-20 19:50:18 +0100

    Write autoload

--- autoload.php
+++ autoload.php
@@ -1,3 +1,8 @@
 <?php
 
-// your autoloader
+$autoload = function($className)
+{
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'src/' . str_replace('/', '\\', $className) . '.php';
+};
+
+spl_autoload_register($autoload);
