e938153b3f227cef1b424c4725ab3b2c08410bb3
nicobascon <nico.bascon@live.fr>
2014-01-26 17:18:38 +0100

    Modify autoload.php file

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
 $autoload = function($className)
 {
-    require_once __DIR__ . DIRECTORY_SEPARATOR . 'src/' . str_replace('/', '\\', $className) . '.php';
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'src/' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
 };
 
 spl_autoload_register($autoload);
