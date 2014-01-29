1fd805572f55ee2d4f303d37be5dea8db80d399a
nicobascon <nico.bascon@live.fr>
2014-01-29 09:28:05 +0100

    Modify routes to get status and statuses

--- app/app.php
+++ app/app.php
@@ -3,6 +3,8 @@
 require __DIR__ . '/../autoload.php';
 
 use Model\InMemoryFinder;
+use Model\JsonFinder;
+use Model\Status;
 
 // Config
 $debug = true;
@@ -10,6 +12,8 @@ $debug = true;
 $app = new \App(new View\TemplateEngine(
     __DIR__ . '/templates/'
 ), $debug);
+
+$jsonFile = __DIR__ .  DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'statuses.json';
 
 /**
  * Index
@@ -22,14 +26,16 @@ $app->get('/index', function () use ($app) {
     return $app->render('index.php');
 });
 
-$app->get('/statuses', function () use ($app) {
-    $memoryFinder = new InMemoryFinder();
+$app->get('/statuses', function () use ($app, $jsonFile) {
+    // $memoryFinder = new InMemoryFinder();
+    $memoryFinder = new JsonFinder($jsonFile);
     $statuses = $memoryFinder->findAll();
     return $app->render('statuses.php', array('array' => $statuses));
 });
 
-$app->get('/statuses/(\d+)', function ($id) use ($app) {
-    $memoryFinder = new InMemoryFinder();
+$app->get('/statuses/(\d+)', function ($id) use ($app, $jsonFile) {
+    // $memoryFinder = new InMemoryFinder();
+    $memoryFinder = new JsonFinder($jsonFile);
     $status = $memoryFinder->findOneById($id);
     return $app->render('status.php', array('item' => $status));
 });
