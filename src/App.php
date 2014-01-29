536b26d233de81046681a899431a06d1bb36e306
nicobascon <nico.bascon@live.fr>
2014-01-29 22:55:08 +0100

    Begin with Request

--- src/App.php
+++ src/App.php
@@ -4,17 +4,10 @@ use Exception\ExceptionHandler;
 use Exception\HttpException;
 use Routing\Route;
 use View\TemplateEngineInterface;
+use Http\Request;
 
 class App
 {
-    const GET    = 'GET';
-
-    const POST   = 'POST';
-
-    const PUT    = 'PUT';
-
-    const DELETE = 'DELETE';
-
     /**
      * @var array
      */
@@ -66,33 +59,33 @@ class App
      */
     public function get($pattern, $callable)
     {
-        $this->registerRoute(self::GET, $pattern, $callable);
+        $this->registerRoute(Request::GET, $pattern, $callable);
 
         return $this;
     }
 
     public function post($pattern, $callable)
     {
-        $this->registerRoute(self::POST, $pattern, $callable);
+        $this->registerRoute(Request::POST, $pattern, $callable);
 
         return $this;
     }
 
     public function put($pattern, $callable)
     {
-        $this->registerRoute(self::PUT, $pattern, $callable);
+        $this->registerRoute(Request::PUT, $pattern, $callable);
 
         return $this;
     }
 
     public function delete($pattern, $callable)
     {
-        $this->registerRoute(self::DELETE, $pattern, $callable);
+        $this->registerRoute(Request::DELETE, $pattern, $callable);
 
         return $this;
     }
 
-    public function run()
+    public function run(Request $request = null)
     {
         $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : self::GET;
         $uri    = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
