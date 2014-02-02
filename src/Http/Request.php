f0c8fd4b9d173fc18705a1f91643028d1411c0d8
nicobascon <nico.bascon@live.fr>
2014-02-02 12:12:45 +0100

    Modify Request class to abstract global variables

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -12,11 +12,22 @@ class Request
 
     const DELETE = 'DELETE';
 
+    private $parameters;
+
+    /**
+     * @param array $query $_GET
+     * @param array $request $_POST
+     */
+    public function __construct(array $query = array(), array $request = array())
+    {
+        $this->parameters = array_merge($query, $request);
+    }
+
     public static function createFromGlobals()
     {
-        return new self();
+        return new self($_GET, $_POST);
     }
-    
+
     public function getMethod()
     {
         $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : self::GET;
@@ -32,5 +43,14 @@ class Request
         }
 
         return $uri;
+    }
+
+    public function getParameter($name, $default = null)
+    {
+        if (array_key_exists($name, $this->parameters)) {
+            return $this->parameters[$name];
+        }
+
+        return $default;
     }
 }
