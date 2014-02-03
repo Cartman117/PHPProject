f6004fc50e8b9c54ec8ff6fed22eb2d8e52ef123
nicobascon <nico.bascon@live.fr>
2014-02-03 20:10:24 +0100

    Add Serializer on GET controllers

--- app/app.php
+++ app/app.php
@@ -6,7 +6,12 @@ use Model\InMemoryFinder;
 use Model\JsonFinder;
 use Model\Status;
 use Http\Request;
+use Http\Response;
 use Exception\HttpException;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 // Config
 $debug = true;
@@ -16,6 +21,9 @@ $app = new \App(new View\TemplateEngine(
 ), $debug);
 
 $jsonFile = __DIR__ .  DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'statuses.json';
+$encoders = array(new XmlEncoder(), new JsonEncoder());
+$normalizers = array(new GetSetMethodNormalizer());
+$serializer = new Serializer($normalizers, $encoders);
 
 /**
  * Index
@@ -28,23 +36,34 @@ $app->get('/index', function () use ($app) {
     return $app->render('index.php');
 });
 
-$app->get('/statuses', function (Request $request) use ($app, $jsonFile) {
+$app->get('/statuses', function (Request $request) use ($app, $jsonFile, $serializer) {
     // $memoryFinder = new InMemoryFinder();
     $memoryFinder = new JsonFinder($jsonFile);
     $statuses = $memoryFinder->findAll();
+    $format = $request->guessBestFormat();
+    if ('html' === $format) {
+        return $app->render('statuses.php', array('array' => $statuses));
+    }
+    $response = new Response($serializer->serialize($statuses, $format));
 
-    return $app->render('statuses.php', array('array' => $statuses));
+    $response->send();
 });
 
-$app->get('/statuses/(\d+)', function (Request $request, $id) use ($app, $jsonFile) {
+$app->get('/statuses/(\d+)', function (Request $request, $id) use ($app, $jsonFile, $serializer) {
     // $memoryFinder = new InMemoryFinder();
     $memoryFinder = new JsonFinder($jsonFile);
     $status = $memoryFinder->findOneById($id);
     if (null === $status) {
         throw new HttpException(404, "Object doesn't exist");
     }
+    $format = $request->guessBestFormat();
+    if ('html' === $format) {
 
-    return $app->render('status.php', array('item' => $status));
+        return $app->render('status.php', array('item' => $status));
+    }
+    $response = new Response($serializer->serialize($status, $format));
+
+    $response->send();
 });
 
 $app->post('/statuses', function (Request $request) use ($app, $jsonFile) {
