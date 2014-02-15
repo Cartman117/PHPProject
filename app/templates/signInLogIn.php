d6fa3fcc2c4b2f08dc18317a269bd5024ae55c12
nicobascon <nico.bascon@live.fr>
2014-02-15 21:41:08 +0100

    Update templates

--- app/templates/signInLogIn.php
+++ app/templates/signInLogIn.php
@@ -1,10 +1,22 @@
-<form action="/signIn" method="GET">
-    <input type="submit" class="submit" value="Sign In"/>
-</form>
-<form action="/logIn" method="GET">
-    <input type="submit" class="submit" value="Log In"/>
-</form>
-<form action="/logOut" method="GET">
-    <input type="submit" class="submit" value="Log Out"/>
-</form>
+<?php
+    if (!isset($_SESSION['username'])) {
+?>
+        <form action="/signIn" method="GET">
+            <input type="submit" class="submit" value="Sign In"/>
+        </form>
+        <form action="/logIn" method="GET">
+            <input type="submit" class="submit" value="Log In"/>
+        </form>
+<?php
+    }
+    if (isset($_SESSION['username'])) {
+        echo $_SESSION['username'] . ' is connected';
+?>
+        <form action="/logOut" method="GET">
+            <input type="submit" class="submit" value="Log Out"/>
+        </form>
+<?php
+    }
+?>
+
 
