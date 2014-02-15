a0f7e0cd345a452fd590ee43ac4c1e14d2821efb
nicobascon <nico.bascon@live.fr>
2014-02-15 18:29:56 +0100

    Add UserQuery class

---
+++ src/Model/UserQuery.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Model;
+
+
+class UserQuery implements FinderInterface
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function findAll()
+    {
+        echo "Function not implemented.";
+    }
+
+    public function findOneById($id)
+    {
+        echo "Function not implemented.";
+    }
+
+    public function findOneByUsername($username)
+    {
+        $query = "SELECT * FROM users WHERE username = :username";
+        $parameters = [':username' => $username];
+
+        $result = $this->connection->executeQuery($query, $parameters);
+        $result = $result->fetch(\PDO::FETCH_ASSOC);
+
+        return ($result !== false) ? new User($result['username'], $result['password']) : null;
+    }
+
+    public function findByUsernameAndPassword($username, $password)
+    {
+        $user = $this->findOneByUsername($username);
+        if (null === $user) {
+            return false;
+        }
+        return ($this->verifyPassword($password, $user->getPassword())) ? true : false;
+    }
+
+    public function logIn($username, $password)
+    {
+        // Not implemented yet.
+    }
+
+    private function verifyPassword($password, $passwordHash)
+    {
+        return password_verify($password, $passwordHash);
+    }
+} 
